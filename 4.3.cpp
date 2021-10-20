f16b6509707e7c8e1f1c959a0c83544072cf7d78
OlehZ17 <91013333+OlehZ17@users.noreply.github.com>
2021-10-20 10:23:50 +0300

    first commit

---
+++ 4.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x + 5 < 0 && c == 0)
+				F = 1 / (a * x) - b;
+			else
+				if (x + 5 > 0 && c != 0)
+					F = (x - a) / x;
+				else
+					F = (10 * x) / (c - 4);
+		y = F;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
